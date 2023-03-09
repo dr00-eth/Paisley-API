1fcc0a071732745c71ce15686f41fe56d086fff4
Andrew Meyer <andrewmeyer23@gmail.com>
2023-03-09 13:41:27 -0600

    update pkgs

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import os
 
 bot = StreamBot(os.getenv('OPENAI_KEY'), "Paisley", genesis_prompt="You are a helpful translator.")
 bot.add_message("Great, you will help me translate some words from English to Spanish.")
-bot.add_message("OK, I will only help you with translations from English to Spanish and reject any other requests", "assistant")
+bot.add_message("OK, I will only help you with translations from English to Spanish and reject any other requests. I will let you know if you violate my rules.", "assistant")
 
 server = StreamBotAPI(bot, origins=["http://localhost:3000","https://paisley-ui-hycvm.ondigitalocean.app"])
 
