6e795783a0f4fa7677361f0ac13fcbf0373eb137
Andrew Meyer <andrewmeyer23@gmail.com>
2023-03-13 17:35:28 -0500

    oops

--- api.py
+++ api.py
@@ -8,15 +8,12 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# Create a file handler and set its level
-file_handler = logging.FileHandler('chatserver.log')
+file_handler = logging.FileHandler('your_log_file.log')
 file_handler.setLevel(logging.DEBUG)
 
-# Create a formatter and add it to the handler
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 
-# Add the handler to the logger
 logger.addHandler(file_handler)
 
 #Prompt 0 - Listing Bot
@@ -25,6 +22,6 @@ streambot1 = StreamBot(os.getenv('OPENAI_KEY'), "Paisley", genesis_prompt=consta
 #Prompt 1 - Neighborhood Bot
 streambot2 = StreamBot(os.getenv('OPENAI_KEY'), "Paisley", genesis_prompt=constants.OPENAI_PROMPT[1])
 
-server = StreamBotAPI([streambot1,streambot2], origins=["http://localhost:3000","https://paisley-ui-hycvm.ondigitalocean.app"], verbosity=1, log_file=logger)
+server = StreamBotAPI([streambot1,streambot2], origins=["http://localhost:3000","https://paisley-ui-hycvm.ondigitalocean.app"], verbosity=1, log_file="chatserver.log")
 
 server.start()
