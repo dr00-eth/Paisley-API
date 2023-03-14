9298cfc04565ceab5ad4a2eec24d57adb1214ff7
Andrew Meyer <andrewmeyer23@gmail.com>
2023-03-14 17:03:05 -0500

    LFG

--- api.py
+++ api.py
@@ -4,18 +4,6 @@ import constants
 import os
 import logging
 
-# Create a logger object
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-file_handler = logging.FileHandler('your_log_file.log')
-file_handler.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-
 #Prompt 0 - Listing Bot
 streambot1 = StreamBot(os.getenv('OPENAI_KEY'), "Paisley", genesis_prompt=constants.OPENAI_PROMPT[0])
 
@@ -28,6 +16,6 @@ streambot3 = StreamBot(os.getenv('OPENAI_KEY'), "Paisley", genesis_prompt=consta
 #Prompt 3 - Follow Up Bot
 streambot4 = StreamBot(os.getenv('OPENAI_KEY'), "Paisley", genesis_prompt=constants.OPENAI_PROMPT[3])
 
-server = StreamBotAPI([streambot1,streambot2, streambot3, streambot4], origins=["http://localhost:3000","https://paisley-ui-hycvm.ondigitalocean.app"], verbosity=1, debug=True, log_file="chatserver.log")
+server = StreamBotAPI([streambot1,streambot2, streambot3, streambot4], origins=["http://localhost:3000","https://paisley-ui-hycvm.ondigitalocean.app"], verbosity=1, debug=False, log_file="chatserver.log")
 
 server.start()
