e8bec522837d77d894ce75b3078a9f0412de0ae6
Andrew Meyer <andrewmeyer23@gmail.com>
2023-03-14 17:49:49 -0500

    temp remove logtail

--- api.py
+++ api.py
@@ -19,6 +19,6 @@ streambot3 = StreamBot(openaikey, "Paisley", genesis_prompt=constants.OPENAI_PRO
 #Prompt 3 - Follow Up Bot
 streambot4 = StreamBot(openaikey, "Paisley", genesis_prompt=constants.OPENAI_PROMPT[3])
 
-server = StreamBotAPI([streambot1,streambot2, streambot3, streambot4], origins=["http://localhost:3000","https://paisley-ui-hycvm.ondigitalocean.app","https://paisley-proto.thegenie.ai"], verbosity=1, debug=False, logtail_key=logtailkey)
+server = StreamBotAPI([streambot1,streambot2, streambot3, streambot4], origins=["http://localhost:3000","https://paisley-ui-hycvm.ondigitalocean.app","https://paisley-proto.thegenie.ai"], verbosity=1, debug=False)
 
 server.start()
