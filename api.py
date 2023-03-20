cdc552288657eb7dff6a109995796a63d1a687ae
Andrew Meyer <andrewmeyer23@gmail.com>
2023-03-20 15:15:30 -0500

    add generic prompt

--- api.py
+++ api.py
@@ -18,6 +18,9 @@ streambot3 = StreamBot(openaikey, "Paisley", genesis_prompt=constants.OPENAI_PRO
 #Prompt 3 - Follow Up Bot
 streambot4 = StreamBot(openaikey, "Paisley", genesis_prompt=constants.OPENAI_PROMPT[3])
 
-server = StreamBotAPI([streambot1,streambot2, streambot3, streambot4], origins=["http://localhost:3000","https://paisley-ui-develop-arxkt.ondigitalocean.app","https://paisley-proto.thegenie.ai"], verbosity=1, debug=False, allow_model_override=True, port=443)
+#Prompt 4 - ChatGPT Generic
+streambot5 = StreamBot(openaikey, "Paisley", genesis_prompt=constants.OPENAI_PROMPT[4])
+
+server = StreamBotAPI([streambot1,streambot2, streambot3, streambot4, streambot5], origins=["http://localhost:3000","https://paisley-ui-develop-arxkt.ondigitalocean.app","https://paisley-proto.thegenie.ai"], verbosity=1, debug=False, allow_model_override=True, port=443)
 
 server.start()
